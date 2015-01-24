ed54188bc51957743619c00fe4f8fd5a9a49e976
impy2gimpy <francisw1990@gmail.com>
2015-01-24 16:59:55 +0200

    added order and color

--- Assets/PlayerActiveScript.cs
+++ Assets/PlayerActiveScript.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class PlayerActiveScript : MonoBehaviour {
 
+	public GameObject playerOne;
+	public GameObject playerTwo;
+	public GameObject playerThree;
+	public GameObject playerFour;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +19,22 @@ public class PlayerActiveScript : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
-
-			renderer.material.color = newColor;
+			 playerOne.renderer.material.color = newColor;
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+			playerTwo.renderer.material.color = newColor;
+		}
+		if (Input.GetMouseButtonDown(2))
+		{
+			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+			playerThree.renderer.material.color = newColor;
+		}
+		if (Input.GetMouseButtonDown(3))
+		{
+			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+			playerFour.renderer.material.color = newColor;
 		}
 	}
 }
