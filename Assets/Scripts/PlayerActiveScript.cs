906713770ce964dabe899fcc1d76d2e5ac6639b0
impy2gimpy <francisw1990@gmail.com>
2015-01-24 18:38:32 +0200

    player select scene

--- Assets/Scripts/PlayerActiveScript.cs
+++ Assets/Scripts/PlayerActiveScript.cs
@@ -8,6 +8,11 @@ public class PlayerActiveScript : MonoBehaviour {
 	public GameObject playerThree;
 	public GameObject playerFour;
 
+	public GameObject playerOneImage;
+	public GameObject playerTwoImage;
+	public GameObject playerThreeImage;
+	public GameObject playerFourImage;
+
 	public int playerOneInt =0;
 	public int playerTwoInt=0;
 	public int playerThreeInt=0;
@@ -31,24 +36,31 @@ public class PlayerActiveScript : MonoBehaviour {
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			 playerOne.renderer.material.color = newColor;
 			playerOneInt=1;
+			playerOneImage.SetActive(true);
 		}
 		if (Input.GetMouseButtonDown(1))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerTwo.renderer.material.color = newColor;
 			playerTwoInt=1;
+			playerTwoImage.SetActive(true);
+
 		}
 		if (Input.GetMouseButtonDown(2))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerThree.renderer.material.color = newColor;
 			playerThreeInt=1;
+			playerThreeImage.SetActive(true);
+
 		}
 		if (Input.GetMouseButtonDown(3))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerFour.renderer.material.color = newColor;
 			playerFourInt=1;
+			playerFourImage.SetActive(true);
+
 		}
 		if (Input.GetKeyDown("space"))
 		{
