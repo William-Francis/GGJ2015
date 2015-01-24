c0584f890c40bc17e3f26203adbb67861a93c17d
Jacques Heunis <jaaksemail@gmail.com>
2015-01-24 18:54:34 +0200

    Give player status for each player individually

--- Assets/Scripts/GlobalController.cs
+++ Assets/Scripts/GlobalController.cs
@@ -1,21 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PlayerState
+{
+    Pending,
+    Joined,
+    Eliminated
+}
+
 public class GlobalController : MonoBehaviour {
 
+    private static GlobalController _instance;
+    public static GlobalController Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
 
-	public static int numberOfPlayers =1;
-	
-	public static void setNumberOfPlayers( int number)
-	{
-		numberOfPlayers = number;
-	}
+    public const int MAX_PLAYER_COUNT = 5;
+    public PlayerState[] playerStates;
 
-	// Use this for initialization
-	void Start () {
-		DontDestroyOnLoad(this.gameObject);
-	}
-	
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        playerStates = new PlayerState[MAX_PLAYER_COUNT];
+        for (int i=0; i<MAX_PLAYER_COUNT; ++i)
+        {
+            playerStates[i] = PlayerState.Pending;
+        }
+    }
+
+    public int playerCount
+    {
+        get
+        {
+            int result = 0;
+            return result;
+        }
+    }
+
+    public void setPlayerState(int player, PlayerState state)
+    {
+        playerStates[player] = state;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown("p"))
@@ -25,9 +65,13 @@ public class GlobalController : MonoBehaviour {
 	}
 
 	void OnLevelWasLoaded(int level) {
-		if (level == 1)
-			print("Woohoo");
-		
+        // Level 0 is the menu level
+        // Level 1 is the inter-level score screen
+        // Levels 2 - n+2 are the n levels
+        if (level > 1)
+        {
+
+        }
 	}
 
 }
--- Assets/Scripts/PlayerActiveScript.cs
+++ Assets/Scripts/PlayerActiveScript.cs
@@ -8,11 +8,6 @@ public class PlayerActiveScript : MonoBehaviour {
 	public GameObject playerThree;
 	public GameObject playerFour;
 
-	public int playerOneInt =0;
-	public int playerTwoInt=0;
-	public int playerThreeInt=0;
-	public int playerFourInt=0;
-
 	//public GlobalController gameController;
 
 	// Use this for initialization
@@ -22,38 +17,33 @@ public class PlayerActiveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//gameController.setNumberOfPlayers(2 );
-
-
-
 		if (Input.GetMouseButtonDown(0))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
-			 playerOne.renderer.material.color = newColor;
-			playerOneInt=1;
+			playerOne.renderer.material.color = newColor;
+            GlobalController.Instance.setPlayerState(0, PlayerState.Joined);
 		}
 		if (Input.GetMouseButtonDown(1))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerTwo.renderer.material.color = newColor;
-			playerTwoInt=1;
+            GlobalController.Instance.setPlayerState(1, PlayerState.Joined);
 		}
 		if (Input.GetMouseButtonDown(2))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerThree.renderer.material.color = newColor;
-			playerThreeInt=1;
+            GlobalController.Instance.setPlayerState(2, PlayerState.Joined);
 		}
 		if (Input.GetMouseButtonDown(3))
 		{
 			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
 			playerFour.renderer.material.color = newColor;
-			playerFourInt=1;
+            GlobalController.Instance.setPlayerState(3, PlayerState.Joined);
 		}
-		if (Input.GetKeyDown("space"))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			GlobalController.setNumberOfPlayers(playerOneInt+playerTwoInt+playerThreeInt+playerFourInt );
-			Application.LoadLevel("testBorderScene");
+			Application.LoadLevel("level1");
 		}
 
 	}
