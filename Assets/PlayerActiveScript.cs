773c4086da9f4267783c6b7909d2240a7de61a86
impy2gimpy <francisw1990@gmail.com>
2015-01-24 16:39:03 +0200

    added button

---
+++ Assets/PlayerActiveScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActiveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+
+			renderer.material.color = newColor;
+		}
+	}
+}
