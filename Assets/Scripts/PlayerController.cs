d4bfa656a1ba70d5f39bd32d3b4bd33fa29beed4
impy2gimpy <francisw1990@gmail.com>
2015-01-25 14:56:09 +0200

    added pig squeals

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,217 +1,3 @@
-<<<<<<< HEAD
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-
-public class PlayerController : MonoBehaviour
-{
-    public int playerID = 0;
-
-    public float glideSpeed = 100;
-    public float floatSpeed = 40;
-	private float weight = -4.1f;
-     private float scale;
-	public GameObject bullet;
-    private float neutralScale = 0.5f; // Store the neutral scale to use as a zero-point for float acceleration
-
-	public AudioClip[] pigSnorts;
-	public AudioClip pigSnort1;
-	public AudioClip pigSnort2;
-	public AudioClip pigSnort3;
-	public AudioClip pigSnort4;
-
-
-	public AudioSource source;
-
-
-	private List<Vector3> bounceList = new List<Vector3> ();
-
-	private float fireRate = 0.1f;
-	private float nextFire = 0.0f;
-
-    public Transform aimIndicator;
-
-	void Awake()
-    {
-	 
-
-		source = GetComponent<AudioSource>();
-
-        scale = rigidbody2D.transform.localScale.x;
-        //neutralScale = scale;
-     }
-
-    void kill()
-    {
-		GlobalController.Instance.playerStates[playerID] = PlayerState.Eliminated;
-		GlobalController.Instance.killPlayer(playerID);
-
-		Destroy(gameObject);
-    }
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.layer == 8) // hazard like spike
-        {
-            kill();
-        }
-		if (coll.gameObject.layer == 9) // bullet
-		{
-			weight+=1.0f;
-			coll.transform.parent = transform;
-			coll.rigidbody.isKinematic=true;
-			coll.collider.enabled=false;
-			rigidbody2D.mass+=1;
-			//add weight here
-			//kill();
-		}
-		if(coll.gameObject.GetComponent<PlayerController>()) // collision with player
-		{
-			switch(Random.Range(0, 4))
-			{
-			case 1:
-				source.PlayOneShot(pigSnort1);
-				break;
-			case 2:
-				source.PlayOneShot(pigSnort2);
-				break;
-			case 3:
-				source.PlayOneShot(pigSnort3);
-				break;
-			case 4:
-				source.PlayOneShot(pigSnort4);
-				break;
-			default:
-				source.PlayOneShot(pigSnort1);
-				break;
-			}
-				 
-
-			 
-			//source.Play();
-			//audio.Play(pigSnort);
-			bounceList.Add(new Vector3(coll.gameObject.rigidbody2D.velocity.x*1.0f,coll.gameObject.rigidbody2D.velocity.y*1.0f,Time.time +1.0f));
-		}  
-	}
-	
-	void FixedUpdate()
-	{
-		//source.Play();
-		//source.PlayOneShot(pigSnort);
-        // 3 - Retrieve axis information
-        float moveX = 0;
-        float moveY = 0;
-        float aimX = 0;
-        float aimY = 0;
-        bool shoot = false;
-
-        switch (playerID)
-        {
-            case(0):
-                moveX = Input.GetAxis("Horizontal");
-                moveY = Input.GetAxis("Vertical");
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector3 direction = mousePosition-transform.position;
-			direction.z=0;
-			direction.Normalize();
-                aimX = direction.x;
-                aimY = direction.y;
-                shoot = Input.GetMouseButtonDown(0);
-                break;
-
-            case(1): case(3):
-                moveX = Input.GetAxis("DPad_XAxis_"+playerID);
-                moveY = Input.GetAxis("DPad_YAxis_"+playerID);
-                aimX = Input.GetAxis("L_XAxis_"+playerID);
-                aimY = Input.GetAxis("L_YAxis_"+playerID);
-                shoot = ((Input.GetAxis("TriggersL_"+playerID) > 0.5f) || (Input.GetButtonDown("LB_"+playerID)));
-                break;
-
-            case (2): case (4):
-                moveX = 0;
-                moveY = 0;
-                if (Input.GetButtonDown("X_"+playerID)) moveX -= 1.0f;
-                if (Input.GetButtonDown("B_"+playerID)) moveX += 1.0f;
-                if (Input.GetButtonDown("Y_"+playerID)) moveY += 1.0f;
-                if (Input.GetButtonDown("A_"+playerID)) moveY -= 1.0f;
-                aimX = Input.GetAxis("R_XAxis_"+playerID);
-                aimY = Input.GetAxis("R_YAxis_"+playerID);
-                shoot = ((Input.GetAxis("TriggersR_"+playerID) > 0.5f) || (Input.GetButtonDown("RB_"+playerID)));
-                break;
-        }
-
-        // Rotate aiming indicator
-        float theta = Mathf.Atan2(aimY, aimX) * Mathf.Rad2Deg;
-        aimIndicator.localRotation = Quaternion.Euler(0, 0, theta-90);
-
-        // Shoot
-		if (shoot  && Time.time >= nextFire)
-		{
-            nextFire = Time.time + fireRate;
-            fireProjectile(aimX, aimY);
-        }
-
-        // 5 - Move the game object
-        // We want scale of 1 to result in 0 upwards acceleration
-        // Clamp scale to a positive value so we don't sink directly due to scale
-        float floatAcceleration = floatSpeed*scale;
-        Vector2 newVelocity = rigidbody2D.velocity;
-        newVelocity.x = glideSpeed*moveX;
-		newVelocity.y = floatAcceleration*(scale-neutralScale) - 4f -weight;
-
-
-		///Adding bounce factor from list
-		for(int i = 0; i < bounceList.Count;i++)
-		{
-			 
-			if(bounceList[i].z <= Time.time)
-			{
-				bounceList.RemoveAt(i);
-				i+=-1;
-			}
-			else{
-
-			newVelocity.x += bounceList[i].x;
-			newVelocity.y += bounceList[i].y;
-			}
-		}
-
-
-		/// 
-        rigidbody2D.velocity = newVelocity;
-
-        scale += 0.5f*sign(moveY)*Time.fixedDeltaTime;
-        scale = Mathf.Clamp(scale, 0.25f, 1.25f);
-
-        rigidbody2D.transform.localScale = new Vector3(scale, scale, scale);
-	}
-
-    float sign(float f)
-    {
-        if (f == 0)
-            return 0.0f;
-        else if (f > 0)
-            return 1.0f;
-        else //if (f < 0)
-            return -1.0f;
-    }
-
-	void fireProjectile(float x, float y)
-	{
-        Vector3 spawnLoc = transform.position;
-        spawnLoc.x += x*scale*1.9f;
-        spawnLoc.y += y*scale*1.9f;
-
-        GameObject bulletInstance = (GameObject)Instantiate(bullet, spawnLoc, Quaternion.identity);
-        // Should the bullet velocity be affected by the player's velocity?
-		Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
-		bulletInstance.renderer.material.color = newColor;
-		bulletInstance.rigidbody2D.AddForce(rigidbody2D.position+(new Vector2(x, y)*500));
-	}
-}
-  
-=======
 using UnityEngine;
 using System.Collections.Generic;
 using XboxCtrlrInput;
@@ -237,6 +23,17 @@ public class PlayerController : MonoBehaviour
 	public AudioClip pigSnort2;
 	public AudioClip pigSnort3;
 	public AudioClip pigSnort4;
+
+	public AudioClip pigDeflate1;
+	public AudioClip pigDeflate2;
+	public AudioClip pigDeflate3;
+	public AudioClip pigDeflate4;
+	public AudioClip pigDeflate5;
+	public AudioClip pigDeflate6;
+	public AudioClip pigDeflate7;
+
+
+
 
 	public AudioSource source;
 
@@ -268,7 +65,35 @@ public class PlayerController : MonoBehaviour
         GlobalController.Instance.playerStates[playerID] = PlayerState.Eliminated;
         GlobalController.Instance.killPlayer(playerID);
 
-        isDead = true;
+		switch(Random.Range(0, 7))
+		{
+		case 1:
+			source.PlayOneShot(pigDeflate1);
+			break;
+		case 2:
+			source.PlayOneShot(pigDeflate2);
+			break;
+		case 3:
+			source.PlayOneShot(pigDeflate3);
+			break;
+		case 4:
+			source.PlayOneShot(pigDeflate4);
+			break;
+		case 5:
+			source.PlayOneShot(pigDeflate5);
+			break;
+		case 6:
+			source.PlayOneShot(pigDeflate6);
+			break;
+		case 7:
+			source.PlayOneShot(pigDeflate7);
+			break;
+		default:
+			source.PlayOneShot(pigDeflate1);
+			break;
+		}
+		
+		isDead = true;
         deadTime = 0.0f;
         deadMoveDir = new Vector3(-rigidbody2D.velocity.x, -rigidbody2D.velocity.y, 0);
         Destroy(gameObject.transform.GetChild(0).gameObject); //Destroy the pointer
@@ -278,6 +103,7 @@ public class PlayerController : MonoBehaviour
 
     void deadUpdate()
     {
+
         deadTime += Time.fixedDeltaTime;
         transform.position += 50*deadMoveDir*Time.fixedDeltaTime;
 
@@ -316,7 +142,24 @@ public class PlayerController : MonoBehaviour
             float otherDeltaBounce = Time.time - otherPlayer.lastBounceTime;
             if (deltaBounce > BOUNCE_THRESHOLD && otherDeltaBounce > BOUNCE_THRESHOLD)
             {
-                source.PlayOneShot(pigSnort1,0.8f);
+				switch(Random.Range(0, 4))
+				{
+				case 1:
+					source.PlayOneShot(pigSnort1);
+					break;
+				case 2:
+					source.PlayOneShot(pigSnort2);
+					break;
+				case 3:
+					source.PlayOneShot(pigSnort3);
+					break;
+				case 4:
+					source.PlayOneShot(pigSnort4);
+					break;
+				default:
+					source.PlayOneShot(pigSnort1);
+					break;
+				}
 
                 lastBounceTime = Time.time;
                 otherPlayer.lastBounceTime = Time.time;
@@ -464,5 +307,4 @@ public class PlayerController : MonoBehaviour
         bulletInstance.rigidbody2D.AddForce(rigidbody2D.position+(new Vector2(x, y)*500));
     }
 
-}
->>>>>>> origin/master
+} 
--- Assets/Scripts/ScoreScreenController.cs
+++ Assets/Scripts/ScoreScreenController.cs
@@ -4,10 +4,39 @@ using System.Collections;
 public class ScoreScreenController : MonoBehaviour {
 
 	public GameObject textOne;
+
+	public AudioClip pigSqueal1; 
+	public AudioClip pigSqueal2; 
+	public AudioClip pigSqueal3; 
+	public AudioClip pigSqueal4; 
+ 
+	public AudioSource source;
+
+
 	// Use this for initialization
 	void Start () {	
 		TextMesh t = (TextMesh)textOne.GetComponent(typeof(TextMesh));
 		t.text =  GlobalController.Instance.scoreToString() ;// + " " + GlobalController.Instance.playerScore.ToString;
+
+		switch(Random.Range(0, 4))
+		{
+		case 1:
+			source.PlayOneShot(pigSqueal1,0.5f);
+			break;
+		case 2:
+			source.PlayOneShot(pigSqueal2,0.5f);
+			break;
+		case 3:
+			source.PlayOneShot(pigSqueal3,0.5f);
+			break;
+		case 4:
+			source.PlayOneShot(pigSqueal4,0.5f);
+			break;
+		default:
+			source.PlayOneShot(pigSqueal1,0.5f);
+			break;
+		}
+
 	}
 	
 	// Update is called once per frame
